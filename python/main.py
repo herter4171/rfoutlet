7d2120e05c51177af98aaa773f6f00f6ee84d4e8
herter4171 <jwhh91@gmail.com>
2019-09-23 17:48:15 -0700

    Add exceptions for webpage.

--- python/main.py
+++ python/main.py
@@ -9,19 +9,26 @@ def index():
     # Print heading and sensor data
     html_str = "<h1>Plant Status</h1>"
     html_str += "<h2>Sensor Data</h2>"
-    html_str += get_table()
 
-    # Show latest plant photo
-    html_str += "<h2>Latest Photo</h2>"
+    try:
+        html_str += get_table()
+    except:
+        html_str += "Data not present.  Is the program running on the Raspberry Pi?"
 
-    pp = photo_parse()
-    img_path = pp.get_latest_path()
-    print(img_path)
+    try:
+        # Show latest plant photo
+        html_str += "<h2>Latest Photo</h2>"
 
-    # Add image tag to HTML
-    html_str += "<img src=\"/{}\" width=\"500\"/>".format(img_path)
-    import os
-    print(os.getcwd())
+        pp = photo_parse()
+        img_path = pp.get_latest_path()
+        print(img_path)
+
+        # Add image tag to HTML
+        html_str += "<img src=\"/{}\" width=\"500\"/>".format(img_path)
+        import os
+        print(os.getcwd())
+    except:
+        html_str += "Image not present.  Has one been uploaded?"
 
     return html_str
 
