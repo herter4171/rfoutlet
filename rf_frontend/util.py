6ec79009b60167efee2fdc6e3441f7a655b414c3
herter4171 <jwhh91@gmail.com>
2019-10-06 14:20:48 -0700

    Fix string exception.

--- rf_frontend/util.py
+++ rf_frontend/util.py
@@ -4,7 +4,8 @@ from pytz import timezone
 def utc_epoch_to_timestamp(epoch_time, local_zone='US/Pacific'):
     """Converts a UNIX time stamp to a time stamp for US/Pacific"""
     utc_time = datetime.fromtimestamp(epoch_time, tz=timezone('UTC'))
-    local_time = utc_time.astimezone(tz=timezone(local_zone))
+    local_time = str(utc_time.astimezone(tz=timezone(local_zone)))
 
     # Remove the seconds trailing, and append time zone PST
-    return local_time[:local_time.rfind(':')] + local_zone
+    local_time = local_time[:local_time.rfind(':')]
+    return ' '.join([local_time, local_zone])
